c3e8b438f1d041035719442aa1427daeaed8faaf
Pieter Delbeke <pieter.delbeke@fabtiek.be>
2021-11-02 15:04:05 +0100

    baseline thebook page

---
+++ src/js/slider.js
@@ -0,0 +1,84 @@
+var slideIndex = 1
+var xDown = null;                                                        
+var yDown = null;
+
+var slider = document.querySelector('.slideshow-container')
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n)
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlideByName(name) {
+    var imgs = document.querySelectorAll('.slideshow-container .mySlides > img')
+    for (i = 0; i < imgs.length; i++) {
+        if (imgs[i].src.indexOf(name) !== -1) {
+            currentSlide(i + 1)
+            return
+        }
+    }
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides")
+    var dots = document.getElementsByClassName("dot")
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    slides[slideIndex - 1].style.display = "block"
+    dots[slideIndex - 1].className += " active"
+}
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches  // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    var firstTouch = getTouches(evt)[0]                               
+    xDown = firstTouch.clientX                               
+    yDown = firstTouch.clientY                               
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return
+    }
+
+    var xUp = evt.touches[0].clientX                                 
+    var yUp = evt.touches[0].clientY
+
+    var xDiff = xDown - xUp
+    var yDiff = yDown - yUp
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            plusSlides(1)
+        } else {
+            plusSlides(-1)
+        }                       
+    }
+    
+    /* reset values */
+    xDown = null
+    yDown = null                                          
+}
+
+slider.addEventListener('touchstart', handleTouchStart, false)
+slider.addEventListener('touchmove', handleTouchMove, false)
+showSlides(slideIndex)
