bb653cf6df6fa1ec4065a78076b0e58693d091e1
Pieter Delbeke <pieter.delbeke@fabtiek.be>
2021-11-24 09:16:02 +0100

    improved the book page

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,10 +1,17 @@
 window.addEventListener('load', function () {
+  var $nav = document.querySelector('nav')
+
+  if (this.window.document.body.getAttribute('nohero') === 'true') {
+    $nav.className = 'scrolled'
+    // disable scroll effect
+    return;
+  }
+
   var mobile = window.document.body.offsetWidth < 768
   window.addEventListener('resize', function () {
     mobile = window.document.body.offsetWidth < 768
   })
 
-  var $nav = document.querySelector('nav')
   window.addEventListener('scroll', function (ev) {
     var st = window.scrollY
     if ((mobile && st > 0) || (st > 24)) {
