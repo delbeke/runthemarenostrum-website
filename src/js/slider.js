ff8d7f1f02337a7685c2870f2d8aab97fe18add2
Pieter Delbeke <pieter.delbeke@fabtiek.be>
2024-06-05 20:18:06 +0200

    content changes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,38 +1,65 @@
 window.addEventListener('load', function () {
-    var slideIndex = 1;
+    var slideIndex = [];
     var xDown = null;                                                        
     var yDown = null;
     
-    var slider = document.querySelector('.slideshow-container')
+    var sliders = document.querySelectorAll('.slideshow-master')
+    for (let i = 0; i < sliders.length; i++) {
+        slideIndex[i] = 0
+    }
+
+    let findIndex = function (arr, item) {
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i] === item) {
+                return i
+            }
+        }
+        return -1
+    }
     
     // Next/previous controls
-    window.plusSlides = function(n) {
-        showSlides(slideIndex += n)
+    window.plusSlides = function(n, sender) {
+        let master = sender.closest('.slideshow-master')
+        let idx = findIndex(sliders, master)
+        slideIndex[idx] += n
+
+        // validate new slideIndex
+        var slides = master.getElementsByClassName("mySlides")
+        if (slideIndex[idx] >= slides.length) {
+            slideIndex[idx] = 0
+        }
+        if (slideIndex[idx] < 0) {
+            slideIndex[idx] = slides.length - 1
+        }
+
+        // make change
+        showSlides(master)
     }
     
     // Thumbnail image controls
-    window.currentSlide = function(n) {
-        showSlides(slideIndex = n)
+    window.currentSlide = function(n, sender) {
+        let master = sender.closest('.slideshow-master')
+        let idx = findIndex(sliders, master)
+        slideIndex[idx] = n
+
+        // make change
+        showSlides(master)
     }
     
-    function showSlides(n) {
+    function showSlides(master) {
         var i;
-        var slides = document.getElementsByClassName("mySlides")
-        var dots = document.getElementsByClassName("dot")
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
+        var slides = master.getElementsByClassName("mySlides")
+        var dots = master.getElementsByClassName("dot")
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none"
         }
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "")
         }
-        slides[slideIndex - 1].style.display = "block"
-        dots[slideIndex - 1].className += " active"
+
+        let idx = findIndex(sliders, master)
+        slides[slideIndex[idx]].style.display = "block"
+        dots[slideIndex[idx]].className += " active"
     }
     
     function getTouches(evt) {
@@ -59,9 +86,9 @@ window.addEventListener('load', function () {
     
         if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
             if ( xDiff > 0 ) {
-                plusSlides(1)
+                plusSlides(1, evt.target)
             } else {
-                plusSlides(-1)
+                plusSlides(-1, evt.target)
             }                       
         }
         
@@ -70,9 +97,12 @@ window.addEventListener('load', function () {
         yDown = null                                          
     }
     
-    if (slider) {
-        slider.addEventListener('touchstart', handleTouchStart, false)
-        slider.addEventListener('touchmove', handleTouchMove, false)
-        showSlides(slideIndex)
+    if (sliders && sliders.length > 0) {
+        for (let i = 0; i < sliders.length; i++) {
+            const sliderContainer = sliders[i].querySelector('.slideshow-container')
+            sliderContainer.addEventListener('touchstart', handleTouchStart, false)
+            sliderContainer.addEventListener('touchmove', handleTouchMove, false)
+            showSlides(sliders[i])
+        }
     }
 })
